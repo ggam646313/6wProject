f7b22bdc02abbb397bda2b3a94b3538106b0e47e
ggam <wjstmdgusz@naver.com>
2019-11-05 17:19:10 +0900

    6wproject 1105

---
+++ app/src/main/java/com/ggamstudio/a6wproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ggamstudio.a6wproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickLogin(View view) {
+    }
+}
